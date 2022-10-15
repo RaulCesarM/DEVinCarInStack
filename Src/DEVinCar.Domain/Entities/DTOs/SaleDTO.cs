89034924e5edd85050c18b37c0c0c67cd78ce5c7
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-10-14 22:30:15 -0300

    Fix: Removendo campos desnecessarios

--- Src/DEVinCar.Domain/Entities/DTOs/SaleDTO.cs
+++ Src/DEVinCar.Domain/Entities/DTOs/SaleDTO.cs
@@ -5,8 +5,7 @@ namespace DEVinCar.Domain.Entities.DTOs
 {
     public class SaleDTO
     {
-        public int Id {get; set;}
-        [Required(ErrorMessage = "The BuyerId is required.")]
+        public int Id {get; set;}        
         public int BuyerId { get; set; }
         public DateTime SaleDate { get; set; }
 
