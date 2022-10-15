eb77b8813a89a0bab3cc630a4fe7bd7bfe0828b1
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-10-14 22:14:39 -0300

    Feature: Criando CarDTOValiations

--- Src/DEVinCar.Domain/Validations/FluentValidations/CarDTOValidators.cs
+++ Src/DEVinCar.Domain/Validations/FluentValidations/CarDTOValidators.cs
@@ -11,7 +11,7 @@ namespace DEVinCar.Domain.Validations.FluentValidations
             RuleFor(x => x.Name)
             .NotEmpty().WithMessage("The name is required")
             .Length(3, 50).WithMessage("Car name must be a maximum of 50 characters");
-            
+
         }
     }
 }
