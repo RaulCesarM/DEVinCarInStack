c06451503a59793f6a8448da83f2ee91c1904237
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-10-14 22:51:12 -0300

    Fix: Removendo campos desnecessarios

--- Src/DEVinCar.Domain/Entities/DTOs/StateDTO.cs
+++ Src/DEVinCar.Domain/Entities/DTOs/StateDTO.cs
@@ -7,15 +7,11 @@ namespace DEVinCar.Domain.Entities.DTOs
     public class StateDTO {
 
         public int Id {get; set;}       
-        public string Name { get; set; }
-        [Required(ErrorMessage ="The initiais is required.")]
-        [MaxLength(2,ErrorMessage= "State initials must be a maximum of 2 characters.")]
+        public string Name { get; set; }        
         public string Initials { get; set; }
-
         public StateDTO()
         {
         }
-
         public StateDTO(State state)
         {
             Id = state.Id;
