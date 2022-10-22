f6a7bc4248a08ff9e164421d87d404beb7b49363
Raul Cesar Mulerschat <raulzito737@gmail.com>
2022-10-22 20:37:18 -0300

    Feature : VERSIONAMENTO

--- Src/DEVinCar.Di/IOC/AuthenticationIOC.cs
+++ Src/DEVinCar.Di/IOC/AuthenticationIOC.cs
@@ -20,7 +20,7 @@ namespace DEVinCar.Di.IOC
         {
             
             IConfiguration configuration = new ConfigurationBuilder()
-                            .SetBasePath(Directory.GetCurrentDirectory())
+                         //   .SetBasePath(Directory.GetCurrentDirectory())
                             .AddJsonFile("appsettings.json")
                             .Build();
 
